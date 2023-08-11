852d9cef2847355939220a5a6797bcdc30d2ca0d
RafaelRufino <rafaelrufino.52@gmail.com>
2023-08-11 18:13:46 -0300

    fix: remove property  Number

--- src/app/trips/[tripId]/confirmation/page.tsx
+++ src/app/trips/[tripId]/confirmation/page.tsx
@@ -79,7 +79,7 @@ const TripConfirmation = ({ params }: { params: { tripId: string } }) => {
   };
   const startDate = new Date(searchParams.get("startDate") as string);
   const endDate = new Date(searchParams.get("endDate") as string);
-  const guests = Number(searchParams.get("guests"));
+  const guests = searchParams.get("guests");
 
   return (
     <div className="container mx-auto p-5">
