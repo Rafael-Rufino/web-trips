69cce5b1eca1b9ad26b1dc940d66d86254ff97af
RafaelRufino <rafaelrufino.52@gmail.com>
2023-08-16 17:34:14 -0300

    refactor: refatorar componente modal

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,9 +1,35 @@
 import React from "react";
 
-const Loading = () => {
+interface LoadingProps {
+  size?: "small" | "medium" | "large" | "xlarge";
+  label?: string;
+  borderColor?: "primary" | "secondary";
+}
+
+const Loading = ({
+  size = "xlarge",
+  label,
+  borderColor = "primary",
+}: LoadingProps) => {
+  const sizes = {
+    small: "h-5 w-5",
+    medium: "h-8 w-8",
+    large: "h-16 w-16",
+    xlarge: "h-32 w-32",
+  };
+
+  const borderColors = {
+    primary: "border-primary",
+    secondary: "border-gray-900",
+  };
   return (
-    <div className="flex justify-center items-center h-screen">
-      <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-900"></div>
+    <div className={`flex items-center justify-center ${!label && "h-screen"}`}>
+      <div className="relative flex justify-center items-center">
+        <div
+          className={`absolute z-20 flex justify-center items-center animate-spin rounded-full ${sizes[size]} border-t-2 border-b-2 ${borderColors[borderColor]} }`}
+        />
+        {label && <span className="relative z-10">{label}</span>}
+      </div>
     </div>
   );
 };
