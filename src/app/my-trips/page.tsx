2fdf601f4b418202ebba39ea675f211ab00e3986
RafaelRufino <rafaelrufino.52@gmail.com>
2023-08-17 02:08:41 -0300

    feat: add validation if you don't have trip booked

--- src/app/my-trips/page.tsx
+++ src/app/my-trips/page.tsx
@@ -5,8 +5,10 @@ import { Prisma } from "@prisma/client";
 import { useEffect, useState } from "react";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 import UserReservationCard from "./components/userReservationCard";
+import Button from "@/components/button";
 
 const MyTrips = () => {
   const [reservations, setReservations] = useState<
@@ -37,17 +39,34 @@ const MyTrips = () => {
     fetchReservations();
   }, [status]);
 
+  const hasReservations = reservations.length > 0;
+
   return (
     <div className="container mx-auto p-5">
-      <h1 className="font-semibold text-primaryDark text-xl">Minhas Viagens</h1>
+      <h1 className=" font-semibold text-primaryDark text-xl">
+        Minhas Viagens
+      </h1>
+      <div className=" flex flex-col w-full h-full min-h-[500px] justify-center">
+        {hasReservations ? (
+          reservations?.map((reservation) => (
+            <UserReservationCard
+              key={reservation.id}
+              reservation={reservation}
+              fetchReservations={() => ({})}
+            />
+          ))
+        ) : (
+          <div className="flex flex-col justify-center items-center h-full ">
+            <p className="text-primaryDark text-lg font-medium mt-2">
+              Você ainda não possui nenhuma viagem
+            </p>
 
-      {reservations?.map((reservation) => (
-        <UserReservationCard
-          key={reservation.id}
-          reservation={reservation}
-          fetchReservations={() => ({})}
-        />
-      ))}
+            <Link href="/my-trips">
+              <Button className="w-full mt-2">Fazer reserva</Button>
+            </Link>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
